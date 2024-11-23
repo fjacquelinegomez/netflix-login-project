f0d9e1806cf785e6cd6c6f11e3dce384843d7a97
Fernanda Gomez <77248726+fjacquelinegomez@users.noreply.github.com>
2024-11-22 21:43:40 -0800

    added troubleshooting line in .py file

--- userdata.py
+++ userdata.py
@@ -7,7 +7,7 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 #gets database url
-DATABASE_URL = os.environ.get('DATABASE_URL')
+DATABASE_URL = os.environ.get['DATABASE_URL']
 
 #connect to the database
 connect = psycopg2.connect(DATABASE_URL, sslmode ='require')
@@ -23,13 +23,16 @@ def login():
     username = request.form.get('username')
     password = request.form.get('password')
     
-    #add user data to the database
-    with connect.cursor() as cur:
-        cur.execute("INSERT INTO users (username, password) VALUES (%s, %s)", (username, password))
-        connect.commit()
-    
-    # Send a response back to the user
-    return "Login data received."
+    try:
+        #add user data to the database
+        with connect.cursor() as cur:
+            cur.execute("INSERT INTO users (username, password) VALUES (%s, %s)", (username, password))
+            connect.commit()
+        
+        return "Login data received."
+    except Exception as e:
+        return "An error has occured: {e}"
+
 
 #if __name__ == '__main__':
     #app.run(debug=True)
